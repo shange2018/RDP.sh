d0c41d58b3e6fc4abb7ed83db222e265f59d92d3
shange2018 <81846986+shange2018@users.noreply.github.com>
2021-04-17 22:10:25 +0800

    Create RDP.sh
    更换ubuntu默认桌面

--- RDP.sh
+++ RDP.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
-{
+#{
 sudo useradd -m N3LK
 sudo adduser N3LK sudo
 echo 'N3LK:0789' | sudo chpasswd
@@ -10,12 +10,15 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
-sudo apt install xfce4-terminal
-sudo apt remove gnome-terminal
 sudo apt install net-tools # ifconfig
 sudo apt install iputils-ping # ping
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+#安装xfce4桌面
+#sudo apt install --assume-yes xfce4 desktop-base
+#sudo apt install xfce4-terminal
+#sudo apt remove gnome-terminal
+#sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+#安装ubuntu默认桌面
+sudo apt-get install ubuntu-desktop
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -53,7 +56,7 @@ sudo bash -c 'echo LC_ALL=\"zh_CN.UTF-8\" >> /etc/environment'
 #修改环境文件(环境变量(所有用户))
 sudo bash -c 'echo LANG=\"zh_CN.UTF-8\" >> /etc/profile'
 sudo adduser N3LK chrome-remote-desktop
-} &> /dev/null &&
+#} &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
